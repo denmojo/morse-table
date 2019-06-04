3689425b409eb85ef2dc3d89b8dead613d4eae01
Dennis Mojado <denmojo@gmail.com>
2019-06-03 21:18:35 -0700

    Moved to friendly Arduino folder

--- beacon/beacon.ino
+++ beacon/beacon.ino
@@ -4,55 +4,54 @@
 // https://brainwagon.org
 //
 
-
 struct t_mtab { char c, pat; } ;
 
 struct t_mtab morsetab[] = {
-  	{'.', 106},
-	{',', 115},
-	{'?', 76},
-	{'/', 41},
-	{'A', 6},
-	{'B', 17},
-	{'C', 21},
-	{'D', 9},
-	{'E', 2},
-	{'F', 20},
-	{'G', 11},
-	{'H', 16},
-	{'I', 4},
-	{'J', 30},
-	{'K', 13},
-	{'L', 18},
-	{'M', 7},
-	{'N', 5},
-	{'O', 15},
-	{'P', 22},
-	{'Q', 27},
-	{'R', 10},
-	{'S', 8},
-	{'T', 3},
-	{'U', 12},
-	{'V', 24},
-	{'W', 14},
-	{'X', 25},
-	{'Y', 29},
-	{'Z', 19},
-	{'1', 62},
-	{'2', 60},
-	{'3', 56},
-	{'4', 48},
-	{'5', 32},
-	{'6', 33},
-	{'7', 35},
-	{'8', 39},
-	{'9', 47},
-	{'0', 63}
+  {'.', 106},
+  {',', 115},
+  {'?', 76},
+  {'/', 41},
+  {'A', 6},
+  {'B', 17},
+  {'C', 21},
+  {'D', 9},
+  {'E', 2},
+  {'F', 20},
+  {'G', 11},
+  {'H', 16},
+  {'I', 4},
+  {'J', 30},
+  {'K', 13},
+  {'L', 18},
+  {'M', 7},
+  {'N', 5},
+  {'O', 15},
+  {'P', 22},
+  {'Q', 27},
+  {'R', 10},
+  {'S', 8},
+  {'T', 3},
+  {'U', 12},
+  {'V', 24},
+  {'W', 14},
+  {'X', 25},
+  {'Y', 29},
+  {'Z', 19},
+  {'1', 62},
+  {'2', 60},
+  {'3', 56},
+  {'4', 48},
+  {'5', 32},
+  {'6', 33},
+  {'7', 35},
+  {'8', 39},
+  {'9', 47},
+  {'0', 63}
 } ;
 
 #define N_MORSE  (sizeof(morsetab)/sizeof(morsetab[0]))
 
-#define SPEED  (12)
+#define SPEED  (18)
 #define DOTLEN  (1200/SPEED)
 #define DASHLEN  (3*(1200/SPEED))
 
@@ -116,11 +115,13 @@ sendmsg(char *str)
 void setup() {
   pinMode(LEDpin, OUTPUT) ;
   Serial.begin(9600) ;
-  Serial.println("Simple Arduino Morse Beacon v0.1") ;
+  Serial.println("Simple Arduino Morse Beacon v0.0") ;
+  Serial.println("Written by Mark VandeWettering <k6hx@arrl.net>") ;
+  Serial.println("Check out my blog @ https://brainwagon.org") ;
   Serial.println("") ;
 }
 
 void loop() {
   sendmsg("VVV DE AD6DM/B CM98HB") ;
-  delay(3000) ;
+  delay(5000) ;
 }
